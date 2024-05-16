ab1c4429a907b3587ee7e83a455fce9f781d9f4c
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-16 13:10:27 +0100

    Created react native app and basic styling with components

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const PlaceholderImage = require(`./assets/alec.png`);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <Text
+        style={{
+          color: "blue",
+          textAlign: "center",
+        }}
+      >
+        Oh I love Hockey so much
+      </Text>
+      <View style={styles.footerContainer}>
+        <Button label="Press if you like hockey" dislike="" />
+        <Button label="Press if you don't like hockey" dislike="don't" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
